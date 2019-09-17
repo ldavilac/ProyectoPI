f3b6378dababbd0ce68a373f36501ff7a52acb8b
elf <elf@gmail.com>
2019-09-16 20:07:14 -0500

    primer-guardado

---
+++ SistemaVentasPI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SistemaVentasPI.Startup))]
+namespace SistemaVentasPI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
